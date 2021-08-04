013a395e3221e53818db593c01a1c1d526c892bf
aimer <99410271@qq.com>
2021-08-04 17:22:31 +0800

    version 1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,6 +237,10 @@
       }
     }
   });
+  let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
 
   /**
    * Animation on scroll
